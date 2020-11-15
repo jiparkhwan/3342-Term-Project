b6cb5a2e7a5de62c58c37e63fa76cc59b789bd90
Alex Drogo <alexmd2918@gmail.com>
2020-11-15 02:10:28 -0500

    Added SP for find all, search name, and random select

--- ClassLibrary/StoredProcedures.cs
+++ ClassLibrary/StoredProcedures.cs
@@ -16,12 +16,149 @@ namespace ClassLibrary
         SqlCommand objCommand = new SqlCommand();
         string strSQL;
 
+        //Displays all actors in database
         public DataSet getAllActors()
         {
             objCommand.CommandType = CommandType.StoredProcedure;
-            objCommand.CommandText = "GetAllActors";
+            objCommand.CommandText = "TP_GetAllActors";
 
             DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getAllMovies()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllMovies";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getAllShows()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllShows";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getAllGames()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllGames";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getRandMovie()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetRandomMovie";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getRandShow()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetRandomShow";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        public DataSet getRandGame()
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetRandomGame";
+
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        //Finds actors based on name typed in search bar. Uses like to find names that are similar as well.
+        public DataSet getActorByName(string actorName)
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetActorsByName";
+
+            SqlParameter inputParameter = new SqlParameter("@Actor_Name", actorName);
+            inputParameter.Direction = ParameterDirection.Input;
+            inputParameter.SqlDbType = SqlDbType.VarChar;
+
+            objCommand.Parameters.Add(inputParameter);
+
+            objDB = new DBConnect();
+            DataSet myDS;
+            myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        //Finds TV shows based on name typed in search bar. Uses like to find titles that are similar as well.
+        public DataSet getShowByName(string showName)
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetTVShowByName";
+
+            SqlParameter inputParameter = new SqlParameter("@Show_Name", showName);
+            inputParameter.Direction = ParameterDirection.Input;
+            inputParameter.SqlDbType = SqlDbType.VarChar;
+
+            objCommand.Parameters.Add(inputParameter);
+
+            objDB = new DBConnect();
+            DataSet myDS;
+            myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        //Finds video games based on name typed in search bar. Uses like to find titles that are similar as well.
+        public DataSet getGameByName(string gameName)
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetVideoGameByName";
+
+            SqlParameter inputParameter = new SqlParameter("@Game_Name", gameName);
+            inputParameter.Direction = ParameterDirection.Input;
+            inputParameter.SqlDbType = SqlDbType.VarChar;
+
+            objCommand.Parameters.Add(inputParameter);
+
+            objDB = new DBConnect();
+            DataSet myDS;
+            myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            return myDS;
+        }
+
+        //Finds movies based on name typed in search bar. Uses like to find titles that are similar as well.
+        public DataSet getMovieByName(string movieName)
+        {
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetMovieByName";
+
+            SqlParameter inputParameter = new SqlParameter("@Movie_Name", movieName);
+            inputParameter.Direction = ParameterDirection.Input;
+            inputParameter.SqlDbType = SqlDbType.VarChar;
+
+            objCommand.Parameters.Add(inputParameter);
+
+            objDB = new DBConnect();
+            DataSet myDS;
+            myDS = objDB.GetDataSetUsingCmdObj(objCommand);
 
             return myDS;
         }
